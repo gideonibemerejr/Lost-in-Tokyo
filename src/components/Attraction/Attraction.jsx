288884a3dcd62bb7c04a0681eb5ba487b6965db4
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-07-09 14:18:11 -0500

    Last update. Project complete

--- src/components/Attraction/Attraction.jsx
+++ src/components/Attraction/Attraction.jsx
@@ -15,17 +15,17 @@ class Attraction extends React.Component {
     })
   }
   render() {
-    const { title, description, className, image } = this.props
-    const { showInfo } = this.state
     return (
       <div
-        className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
+        className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${
+          this.props.className
+        }`}
         onMouseEnter={this.toggleInfo}
         onMouseLeave={this.closeInfo}
       >
         <div className="relative">
           <Overlay {...this.props} {...this.state} />
-          <img alt="" src={`../images/${image}`} className="db" />
+          <img alt="" src={`../images/${this.props.image}`} className="db" />
         </div>
       </div>
     )
--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavItem = ({ className, href, children, logo }) => (
   <li className={`mh2-ns f6 f4-l tc ${className}`}>
-    <a className="white no-underline" href={href}>
+    <a className="white no-underline" href=" ">
       {' '}
       {logo ? (
         <img src="./images/logo.svg" className="db center logo" alt="Logo" />
